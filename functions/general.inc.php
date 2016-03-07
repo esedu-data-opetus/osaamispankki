a917fa241bf69d97a01ac8a8637d25ca315bb2db
tumelius <tumelius>
2016-03-07 11:01:42 +0200

    poistettiin masterista kaikki roju

--- functions/general.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-function outputErrors($errors) {
-	$output = '<p style="color:red">' . implode('</li><li>', $errors) . '</p>';
-	return $output;
-}
--- tiedot/core/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'functions/users.php'; 
-session_start();
-if (!isset($_SESSION['user'])) 
-	header('Location: ../login.php');
