c68c56dedbda112d01ee04fa94fa3d82607b84b7
Riku <riku.ronka@esedulainen.fi>
2016-03-15 12:30:21 +0200

    header ja footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,9 @@
-	<!--<p class="footer" style="padding:10px;">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php// echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p> -->
+<body>
+<footer class="footer">
+
+<div class="footer navbar-fixed-bottom">
+	<p class="text-muted">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p><p class="text-muted"> Kurieitā: Riku, Miika ja Valtteri</p>
+</div>
+</footer>
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,64 @@
 <link href="<?php echo base_url();?>bootstrap/css/bootstrap.css" rel="stylesheet">
-
-<nav class="navbar navbar-default" style="margin-top:-60px; >
+<style>
+#osaamispankki {
+	
+	background-color:#0066ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+#osaamispankki:hover{
+	background-color: red;
+}
+#luotili {
+background-color:#0066ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+#luotili:hover{
+background-color: red;
+}
+#kirjautuminen{
+background-color:#0066ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+#kirjautuminen:hover{
+background-color: red;	
+}
+#kirjauduulos{
+background-color:#0066ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+#kirjauduulos:hover{
+	background-color: red;
+}
+</style>
+<nav class="navbar navbar-default" style="margin-top:-60px;>
   <div class="container-fluid" style="margin-top:-150px;">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -10,22 +68,45 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Osaamispankki</a>
+      <?php echo '<a class="navbar-brand" href="'.base_url().'index.php/sivu'.'"'?>>Osaamispankki</a> 
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <!--  div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><?php echo '<a href="'.base_url().'index.php/sivu/login'.'"><button type="button" class="btn btn-success" style="margin-top:-10px;width:100px; height:40px; text-size:4px; padding:0px; float: left;" >Kirjautuminen</button></a></li>'?>
-        <li class="active"><?php echo '<a href="'.base_url().'index.php/sivu/register'.' "><button type="button" class="btn btn-success" style="margin-top:-10px;width:100px; height:40px; text-size:4px; padding:0px; float: left;" >Luo tili</button></a></li>'?>
-        
+        <li class="active"><?php echo '<a role="button" class="btn btn-success" href="'.base_url().'index.php/sivu/login'.'">Kirjautuminen</button></a></li>'?>
+        <li class="active"><?php echo '<a role="button" class="btn btn-primary" href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a></li>'?>
+        <li class="active"><?php echo '<a role="button" class="btn btn-danger" href="'.base_url().'index.php/sivu/logout'.' ">Kirjaudu ulos</button></a></li>'?>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
     </div><!-- /.navbar-collapse -->
-<!-- /.container-fluid -->
+  </div><!-- /.container-fluid -->
 </nav>
+
+	  	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<?php
+				$test = $this->session->userdata('');
+				if($this->session->userdata('is_logged_in') == 1)
+						{
+							echo '<a role="button" id="kirjauduulos" style="float:right;" href="'.base_url().'index.php/sivu/logout'.' ">Kirjaudu ulos</button></a>';
+						}
+						elseif($this->uri->segment(2) == "login")
+						{
+							echo '<a role="button" id="osaamispankki"  href="'.base_url().'index.php/sivu'.'">Osaamispankki</a>';
+							echo '<a role="button" id="luotili"  href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
+						
+						}
+						elseif($this->uri->segment(2) == "register")
+						{
+							echo '<a role="button" id="osaamispankki" href="'.base_url().'index.php/sivu'.'">Osaamispankki</a>'; 
+							echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';							
+						}
+						else 
+						{
+							echo '<a role="button" id="kirjautuminen "href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';
+							echo '<a role="button" id="luotili"  href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
+						}
+				?>
+		</nav>
+
+<!--Laitetaan paddingiä jotta navbar ei mene tekstin päälle-->
+<body style="padding-top: 60px;">
