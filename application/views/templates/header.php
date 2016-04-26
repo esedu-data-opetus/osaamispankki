b5473d3720a5d6aeddba6cfd6d0593c76feb5d36
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-04-26 12:24:44 +0300

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,67 +1,79 @@
 <link href="<?php echo base_url();?>bootstrap/css/bootstrap.css" rel="stylesheet">
 <link href="<?php echo base_url();?>bootstrap/css/ownstyles.css" rel="stylesheet">
+<style>
 
-  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<?php
-			// Laittaa napit sen mukaan millä sivulla on
-		$test = $this->session->userdata('');
-		if($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria")
-				{
-					echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
-				}
-				elseif($this->uri->segment(2) == "login")
-				{
-					echo '<a role="button" id="osaamispankki"  href="'.base_url().'">Osaamispankki</a>';
-					echo '<a role="button" id="luotili"  href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
-				
-				}
-				elseif($this->uri->segment(2) == "login_validation")
-				{
-					echo '<a role="button" id="osaamispankki"  href="'.base_url().'">Osaamispankki</a>';
-					echo '<a role="button" id="luotili"  href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
-				
-				}
-				elseif($this->uri->segment(2) == "register")
-				{
-					echo '<a role="button" id="osaamispankki" href="'.base_url().'">Osaamispankki</a>';
-					echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';							 														
-				}
-				elseif($this->uri->segment(2) == "register_validation")
-				{
-					echo '<a role="button" id="osaamispankki" href="'.base_url().'">Osaamispankki</a>';
-					echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';							 														
-				}
-				elseif($this->uri->segment(2) == "members")
-				{
-					echo '<a role="button" id="kirjauduulos" style="float:right;" href="'.base_url().'index.php/sivu/logout'.' ">Kirjaudu ulos</button></a>';
-					echo "Tervetuloa "; 
-					echo "<b style='font-size:15px;'>";
-					echo $this->session->userdata('sposti');
-					echo "</b>";						 														
-				}
-				elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "")
-				{
-					echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Oma profiili</a>';
+</style>
 
-				}
-				elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyokokemus")
-				{
-					echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
-				}
 
-				elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "koulutukset")
-				{
-					echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
-				}
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<?php
+		// Laittaa napit sen mukaan millä sivulla on
+	$test = $this->session->userdata('');
+	if($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria")
+			{
+				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
+			}
+			elseif($this->uri->segment(2) == "login")
+			{
+				echo '<a role="button" id="osaamispankki"   href="'.base_url().'">Osaamispankki</a>';
+				echo '<a role="button" id="luotili"  href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
+			
+			}
+			elseif($this->uri->segment(2) == "login_validation")
+			{
+				echo '<a role="button" id="osaamispankki"  href="'.base_url().'">Osaamispankki</a>';
+				echo '<a role="button" id="luotili"  href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
+			
+			}
+			elseif($this->uri->segment(2) == "register")
+			{
+				echo '<a role="button" id="osaamispankki" href="'.base_url().'">Osaamispankki</a>';
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';							 														
+			}
+			elseif($this->uri->segment(2) == "register_validation")
+			{
+				echo '<a role="button" id="osaamispankki" href="'.base_url().'">Osaamispankki</a>';
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';							 														
+			}
+			elseif($this->uri->segment(2) == "members")
+			{
+				echo '<a role="button" id="kirjauduulos" style="float:right;" href="'.base_url().'index.php/sivu/logout'.' ">Kirjaudu ulos</button></a>';
+				echo "Tervetuloa "; 
+				echo "<b style='font-size:15px;'>";
+				echo $this->session->userdata('sposti');
+				echo "</b>";						 														
+			}
+			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "")
+			{
+				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Oma profiili</a>';
 
-				else
-				{
-					echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';
-					echo '<a role="button" id="luotili" href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
+			}
+			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "edit_tyohistoria")
+			{
+				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
+			}
 
-				}
-				
-		?>
+			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "edit_koulutukset")
+			{
+				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
+			}
+			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria_lisaus")
+			{
+				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
+			}
+			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "koulutukset_lisaus")
+			{
+				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'index.php/sivu/members'.'">Takaisin profiiliin</a>';
+			}
+
+			else
+			{
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'index.php/sivu/login'.' ">Kirjautuminen</button></a>';
+				echo '<a role="button" id="luotili" href="'.base_url().'index.php/sivu/register'.' ">Luo tili</button></a>';
+
+			}
+			
+	?>
 </nav>
 
 <!--Laitetaan paddingiä jotta navbar ei mene tekstin päälle-->
