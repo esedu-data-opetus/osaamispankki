24cadf97fcb8b02143b59645d36d2d9a5502b6a9
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-05-04 09:56:16 +0300

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,14 +6,29 @@
 	<!--<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>-->
 	<meta charset="utf-8">
-	<title>Etusivu</title>
+	<title>ETURIVIN TAITAJAT - Osaajapankki</title>
 </head>
 
 <style type="text/css">
 </style>
 
 <body>
-<h1 style="text-align:center;font-weight:bold;">Etusivu</h1><br>
-<h5 class="col-md-6 col-md-offset-3" style="text-align:center;"><b>Tervetuloa esedun osaamispankkiin</b>Ban bota ttsukasa jō tsui toi ten kyō rō sechiyahe mo asa gimo bu a kyō 7 gi fuyohoha yukihana kara 閉秋 Hachi gare-guchi ryakugiki ma ̄. Tsubuse hori aya-gumi re 企揚 Shihohi san 試 Raru-to nen suroyose ketsu mukashi jun kuweniwa choku 87 jō yoraeho otoko-daitsu betsu zen ~yue dō-yasu ronosaso-ku shutsu n no-sa ku no shio ranahika-ka 82 bun no oi-ban 応角 ~Yo 唆芸Ta shiki ki hai kuku po uwa to. Bun 批 Wenuma hare saru ewoa tsuna mi go re zoku kai dzuha ~yo Mitsugu bō-kiri miyako no yame-sho ichi zaso-dan-naiji sara atama aotafu-fu katachi rē cha nami 46 呆實 Jō 栩 De getto. Akira kukina toi he nōte kuruma-gōre hezu-cho-shin to re be gi-kaku 27-sai dzu kuda~tsuBetsudai wa Sakai 3 Etsu-tai no 予改 Mafuke seki samurai 勃 Po. Azuma n watsu aji dan egu de ken Jinga hi yasuji ze ro kare mi ne ~ya ku-hō dan kuuwa chikaramizu re-za-kō taruhano no 20 Sasō tore Hitoshi Hori re ~yukio ka 鍛-Me yore mi tsukigiri haku tsutsumi wa ho.-Ten mokuchio ten-go 6 dai ra-ryō 必開 Ru Shiki Emi dzuo Hori 69 企 Ku ka ne Mukai shussha ni men-teki pozu e 掲六 Nni bo su-nai-in fuku-ken nottonte. 
+<?php
+echo '<img src="'.base_url().'pictures/esedu_logo.png" class="col-md-5 col-md-offset-4" style="width:250px;height:90px;">';
+echo '<a href ="'.base_url().'index.php/sivu/welcome_message_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5 col-md-offset-6" style="width:75px;height:30px;margin-right:-80px;"></a>';
+
+?>
+
+<h2 class="col-md-5 col-md-offset-4">ETURIVIN TAITAJAT - Osaajapankki</h2><br /><br />
+<div class="col-md-5 col-md-offset-4" >
+<p style="">ETURIVIN TAITAJAT - Osaajapankki on Etelä-savon ammattiopiston (Esedu) ylläpitämä verkkopalvelu, jonka tarkoituksena on edistää opiskelijoiden työllistymistä.</p><br />
+<p>Esedussa opiskelevana voit halutessasi edistää omaa työllistymistäsi lisäämällä osaajapankkiin yhteystietojesi lisäksi omaa koulutustasi ja osaamistasi koskevat tiedot sekä ylläpitää niitä.</p><br />
+<p>- Työtä tarjoavat organisaatiot löytävät parhaat osaajat täältä.</p><br /><br />
+<p>Ohjeet:</p>
+<p>Kirjaudu ETURIVIN TAITAJAT - Osaajapankkiin omalla <b>@esedulainen.fi</b> sähköpostitunnuksellasi, täytä yhteystietosti, omaa koulutustasi, työkokemustasi, osaamistasi ja harrastuksiasi koskevat tiedot, ja pidä ne ajan tasalla. - Voit myöskin lisätä oman CV:n ja osaamisportfoliosi sekä linkin omille kotisivuillesi.</p>
+<p>Tärkeintä on, että kirjaamasi tiedot ovat todenmukaisia, ja siten lisäävät luottamusta sinuun.</p>
+<p>Silloin, kun et ole hakemassa aktiivisesti uutta työpaikkaa, voit poistaa tietosi tai laittaa ne näkymättömäksi.<br /> - Näkymättömäksi laitetut tiedot voit helposti palauttaa aktiiviseksi ja päivittää vastaamaan uutta tilannetta.</p><br /><br />
+<i>ETURIVIN TAITAJAT - Osaajapankkiin kirjatut tiedot ovat kuitenkin Esedussa opiskelevan tai opiskelleen itsensä pankkiin kirjaamia, joten myös vastuu omien tietojen oikeellisuudesta on heillä itsellään. - Esedu ei vastaa Osaajapankkiin kirjatuista tiedoista eikä niiden käytöstä mahdollisesti aiheutuvista seuraamuksista.</i>
+</div>
 </body>
 </html>
