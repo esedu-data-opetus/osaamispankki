fa74f036ec1b1662542922e048a06c0ecec6c6e2
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-05-10 11:57:04 +0300

    Update hakutulokset.php

--- application/views/hakutulokset.php
+++ application/views/hakutulokset.php
@@ -2,7 +2,7 @@
 echo form_open('sivu/hakutulokset');
 $haku = array('value'=> $this->input->post('haku'),'name'=>'haku','id'=>'haku', 'size'=>'40', 'type'=>'text', 'placeholder'=>'Kirjoita haku tähän');
 echo '<br>'.form_input($haku);
-echo form_submit('submit', 'Haku', 'class="btn btn-success"');
+echo form_submit('submit', 'Hae', 'class="btn btn-success"');
 form_close();
 echo '</div>';
 if($this->input->post('haku') == '')
