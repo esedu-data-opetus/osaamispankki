c8ee4f184c4cc55ffe3183f52549c24302bdc115
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-05-10 12:15:27 +0300

    Update Sivu.php

--- application/controllers/Sivu.php
+++ application/controllers/Sivu.php
@@ -520,7 +520,9 @@ class Sivu extends CI_Controller {
 				if ($this->email->send()){
 					echo "<center><h2 style='font-weight:bold;color:green;'>Vahvistus on lähetetty sähköpostiisi!</h2>";
 					echo "<p><a href='".base_url()."index.php/sivu/login' >Takaisin kirjautumiseen</a></p></center>";
-			} 	else echo "<h2 style='font-weight:bold;color:red;'>Sähköpostin lähetys ei onnistu.</h2>";
+			} 	else echo "<h2 style='font-weight:bold;color:red;'>Sähköpostin lähetys ei onnistu localhostilla.</h2>";
+				     echo '<h4>Mutta pystyt kuitenkin kirjautumaan sisään</h4>';
+				     echo "<a href='".base_url()."index.php/sivu/login' >Takaisin kirjautumiseen</a>";
 					
 		} else echo "Ongelma tietokantaan lisätessä.";
 
