5590bd9b5d2890e6e26d0566f857f94b9c8102fe
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-05-10 11:56:49 +0300

    Update haku.php

--- application/views/haku.php
+++ application/views/haku.php
@@ -4,6 +4,6 @@ echo '<div class="col-md-6 col-md-offset-5">';
 echo form_open('sivu/hakutulokset');
 $haku = array('name'=>'haku','id'=>'haku', 'size'=>'40', 'type'=>'text', 'placeholder'=>'Kirjoita haku tähän');
 echo '<br>'.form_input($haku);
-echo form_submit('submit', 'Haku', 'class="btn btn-success"');
+echo form_submit('submit', 'Hae', 'class="btn btn-success"');
 form_close();
 echo '</div>';
