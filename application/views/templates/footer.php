665deef67a5cbe37af5b9c8583ea46c8a4492c1a
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-05-11 10:36:55 +0300

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,8 +5,7 @@
 	<a href="http://www.esedu.fi/" target="_blank" >
 	<?php
 	echo '<img src="'.base_url().'pictures/esedu_logo.png" style="width:２０0px;height:90px;"></a>';
-/*
-	TP was here!
+
 	$test = $this->session->userdata('');
 	if($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria")
 			{
@@ -19,69 +18,69 @@
 			}
 			elseif($this->uri->segment(2) == "login")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/login_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';	
+				echo '<a href ="'.base_url().'sivu/login_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';	
 			}
 			elseif($this->uri->segment(2) == "login_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/login"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/login"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			
 			}
 			elseif($this->uri->segment(2) == "login_validation")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/login_validation_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/login_validation_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "login_validation_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/login_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';	
+				echo '<a href ="'.base_url().'sivu/login_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';	
 			}
 			elseif($this->uri->segment(2) == "register")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/register_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';								 														
+				echo '<a href ="'.base_url().'sivu/register_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';								 														
 			}
 			elseif($this->uri->segment(2) == "register_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/register"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';					 														
+				echo '<a href ="'.base_url().'sivu/register"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';					 														
 			}
 			elseif($this->uri->segment(2) == "register_validation")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/register_validation_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';						 														
+				echo '<a href ="'.base_url().'sivu/register_validation_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';						 														
 			}
 			elseif($this->uri->segment(2) == "register_validation_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/register_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';						 														
+				echo '<a href ="'.base_url().'sivu/register_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';						 														
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "welcome_message")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/welcome_message_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/welcome_message_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "welcome_message_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/welcome_message"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			/*elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "edit_tyohistoria")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/edit_tyohistoria_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/edit_tyohistoria_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "edit_tyohistoria_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/edit_tyohistoria"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/edit_tyohistoria"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "edit_koulutukset")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/edit_koulutukset_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/edit_koulutukset_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "edit_koulutukset_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/edit_koulutukset"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/edit_koulutukset"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria_lisaus")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/tyohistoria_lisaus_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/tyohistoria_lisaus_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria_lisaus_english")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/tyohistoria_lisaus"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/tyohistoria_lisaus"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "koulutukset_lisaus")
 			{
@@ -91,23 +90,23 @@
 			{
 			
 			}*/
-/*
-			elseif($this->uri->segment(2) == "welcome_message")
+
+			elseif($this->uri->segment(2) == "")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/welcome_message_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/welcome_message_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "welcome_message_english")
 			{	
-				echo '<a href ="'.base_url().'index.php/sivu/welcome_message"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "members")
 			{
-				echo '<a href ="'.base_url().'index.php/sivu/members_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+				echo '<a href ="'.base_url().'sivu/members_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "members_english")
 			{	
-				echo '<a href ="'.base_url().'index.php/sivu/members"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
-			}*/
+				echo '<a href ="'.base_url().'sivu/members"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
+			}
 	?>
 	<span class="text-muted">ページ rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></span><span class="text-muted"><br><p style="margin-left:227px;margin-top:-30px;"> <b>Tekijät:</b> リク, ミーカ ja ティキ</p></span>
 	
