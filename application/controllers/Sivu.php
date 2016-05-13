9452e205955b6f20e63bd0aed0f6457b55c621be
LSDsuperfruit <lsdsuperfruit@gmail.com>
2016-05-13 09:18:36 +0300

    Update Sivu.php

--- application/controllers/Sivu.php
+++ application/controllers/Sivu.php
@@ -80,6 +80,16 @@ class Sivu extends CI_Controller {
 
 	}
 	
+	public function index()
+	{
+		if ($this->session->userdata('is_logged_in')){
+		$this->load->template('perustiedot');
+	} else {
+		redirect('sivu/restricted');
+	}
+	
+	}
+	
 	public function members_english()
 	{
 		if ($this->session->userdata('is_logged_in')) {
@@ -164,7 +174,7 @@ class Sivu extends CI_Controller {
 			$this->session->set_userdata($data);
 			
 				if($this->session->userdata('usertype') == 1) {
-					redirect('sivu2');
+					redirect('sivu/paivita_perustiedot');
 				}
 				elseif($this->session->userdata('usertype') == 2){
 					redirect('sivu/members');
@@ -781,4 +791,42 @@ class Sivu extends CI_Controller {
  	redirect('sivu/restricted');
  	}
  	}
+ 	
+ 	public function paivita_perustiedot()
+		{
+		$this->load->model('model_sivu');
+
+		$this->load->library('form_validation');
+		
+		$this->form_validation->set_rules('privSposti', 'Henkilökohtainen sähköpostiosoite', 'valid_email|max_length[30]');
+		$this->form_validation->set_rules('eNimi', 'Etunimi', 'alpha|trim|max_length[30]');
+		$this->form_validation->set_rules('sNimi', 'Sukunimi', 'alpha|trim|max_length[30]');
+		$this->form_validation->set_rules('osoite', 'Osoite', 'trim|max_length[30]');
+		$this->form_validation->set_rules('postinro', 'Postinumero', 'trim');
+		$this->form_validation->set_rules('puhelinnro', 'Puhelinnumero', 'trim|numeric|max_length[12]');
+
+
+		$this->form_validation->set_message('alpha', "<p style='color:red;''>Nimessä voi olla vain aakkosia.</p>");
+		$this->form_validation->set_message('valid_email', "<p style='color:red;''>Sähköpostiosoite ei ole kelvollinen.</p>");
+		$this->form_validation->set_message('max_length', "<p style='color:red;''>Jokin kenttä on liian pitkä</p>");
+		$this->form_validation->set_message('min_length', "<p style='color:red;''>Jokin kenttä on liian lyhyt</p>");
+		$this->form_validation->set_message('numeric', "<p style='color:red;''>Virheellinen puhelinnumero</p>");
+
+
+			if ($this->form_validation->run())
+			{
+
+						if($this->model_sivu->paivitatiedot())
+								{
+									redirect('sivu/members');
+								}
+								else
+								{
+									redirect('sivu/members');
+								}
+			}
+			$this->load->template('perustiedot');
+
+
+		}
 }
