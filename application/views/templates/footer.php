fe2ea6b80026016d5cfd979e43ed91e6c6c1c6f7
Marko Knuutinen <marko.knuutinen@datanomit.local>
2016-11-08 11:49:00 +0200

    Sorting and mobile optimising

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,28 +1,26 @@
 <footer class="footer">
-
 <div class="footer navbar-fixed-bottom">
-	<a href="http://www.esedu.fi/" target="_blank" >
 	<?php
-	echo '<img src="'.base_url().'pictures/esedu_logo.png" style="width:２０0px;height:90px;"></a>';
+	echo '<a href="http://www.esedu.fi/" target="_blank" ><img src="'.base_url().'pictures/esedu_logo.png" style="width:２０0px;height:90px;" /></a>';
 
 	$test = $this->session->userdata('');
 	if($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria")
 			{
-				
+
 			}
 			elseif( $this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria_english")
 			{
-			
-			
+
+
 			}
 			elseif($this->uri->segment(2) == "login")
 			{
-				echo '<a href ="'.base_url().'sivu/login_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';	
+				echo '<a href ="'.base_url().'sivu/login_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "login_english")
 			{
 				echo '<a href ="'.base_url().'sivu/login"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
-			
+
 			}
 			elseif($this->uri->segment(2) == "login_validation")
 			{
@@ -30,23 +28,23 @@
 			}
 			elseif($this->uri->segment(2) == "login_validation_english")
 			{
-				echo '<a href ="'.base_url().'sivu/login_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';	
+				echo '<a href ="'.base_url().'sivu/login_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "register")
 			{
-				echo '<a href ="'.base_url().'sivu/register_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';								 														
+				echo '<a href ="'.base_url().'sivu/register_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "register_english")
 			{
-				echo '<a href ="'.base_url().'sivu/register"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';					 														
+				echo '<a href ="'.base_url().'sivu/register"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "register_validation")
 			{
-				echo '<a href ="'.base_url().'sivu/register_validation_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';						 														
+				echo '<a href ="'.base_url().'sivu/register_validation_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "register_validation_english")
 			{
-				echo '<a href ="'.base_url().'sivu/register_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';						 														
+				echo '<a href ="'.base_url().'sivu/register_validation"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "welcome_message")
 			{
@@ -61,7 +59,7 @@
 				echo '<a href ="'.base_url().'sivu/welcome_message_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "welcome_message_english")
-			{	
+			{
 				echo '<a href ="'.base_url().'"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "members")
@@ -69,12 +67,12 @@
 				echo '<a href ="'.base_url().'sivu/members_english"><img src="'.base_url().'pictures/English_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 			elseif($this->uri->segment(2) == "members_english")
-			{	
+			{
 				echo '<a href ="'.base_url().'sivu/members"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5" style="width:75px;height:30px;float:right;"></a>';
 			}
 	?>
 	<span class="text-muted"><br><p style="margin-left:227px;margin-top:-30px;"> <b>Tekijät:</b> Riku, Miika ja Valtteri</p></span>
-	
+
 
 </div>
 </footer>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,16 +4,16 @@
 	<link href="<?php echo base_url();?>bootstrap/css/bootstrap.css" rel="stylesheet">
 	<link href="<?php echo base_url();?>bootstrap/css/ownstyles.css" rel="stylesheet">
 	<link rel="icon" href="<?=base_url()?>/esedu_logo.ico" type="image/gif">
-	
-	<script>
-	function hideMessage() {
-	    document.getElementById("message").style.display = "none";
-	};
-	setTimeout(hideMessage, 2000);
-	</script>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <a style="margin: 0; padding: 0;" class="navbar-brand" href="<?php echo base_url(); ?>"><img src="http://paja.esedu.fi/op/pictures/esedu_logo.png" style="max-height:50px; margin: 0; padding: 0;"></a>
+	        </div>
+					<div id="navbar" class="collapse navbar-collapse">
+          <ul style="width: auto;" class="nav navbar-nav">
 	<?php
 		// Laittaa napit sen mukaan millä sivulla on
 	$test = $this->session->userdata('');
@@ -24,56 +24,55 @@
 			elseif( $this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "tyohistoria_english")
 			{
 				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'sivu/members_english'.'">Back to profile</a>';
-			
+
 			}
 			elseif($this->uri->segment(2) == "login")
 			{
 				echo '<a role="button" id="osaamispankki"   href="'.base_url().'">Osaamispankki</a>';
 				echo '<a role="button" id="luotili" 		href="'.base_url().'sivu/register'.' ">Luo tili</button></a>';
-			
+
 			}
 			elseif($this->uri->segment(2) == "login_english")
 			{
 				echo '<a role="button" id="osaamispankki"   href="'.base_url().'sivu/welcome_message_english'.'">Learning bank</a>';
 				echo '<a role="button" id="luotili"  		href="'.base_url().'sivu/register_english'.' ">Create an account</button></a>';
-			
+
 			}
 			elseif($this->uri->segment(2) == "login_validation")
 			{
 				echo '<a role="button" id="osaamispankki"  href="'.base_url().'">Osaamispankki</a>';
 				echo '<a role="button" id="luotili" 	   href="'.base_url().'sivu/register'.' ">Luo tili</button></a>';
-			
+
 			}
 			elseif($this->uri->segment(2) == "login_validation_english")
 			{
 				echo '<a role="button" id="osaamispankki"  href="'.base_url().'sivu/welcome_message_english'.'">Learning bank</a>';
 				echo '<a role="button" id="luotili"  	   href="'.base_url().'sivu/register_english'.' ">Create an account</button></a>';
-			
+
 			}
 			elseif($this->uri->segment(2) == "register")
 			{
 				echo '<a role="button" id="osaamispankki" href="'.base_url().'">Osaamispankki</a>';
-				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login'.' ">Kirjautuminen</button></a>';							 														
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login'.' ">Kirjautuminen</button></a>';
 			}
 			elseif($this->uri->segment(2) == "register_english")
 			{
 				echo '<a role="button" id="osaamispankki" href="'.base_url().'sivu/welcome_message_english'.'">Learning bank</a>';
-				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login_english'.' ">Login</button></a>';							 														
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login_english'.' ">Login</button></a>';
 			}
 			elseif($this->uri->segment(2) == "register_validation")
 			{
 				echo '<a role="button" id="osaamispankki" href="'.base_url().'sivu/welcome_message'.'">Osaamispankki</a>';
-				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login'.' ">Kirjautuminen</button></a>';							 														
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login'.' ">Kirjautuminen</button></a>';
 			}
 			elseif($this->uri->segment(2) == "register_validation_english")
 			{
 				echo '<a role="button" id="osaamispankki" href="'.base_url().'sivu/welcome_message_english'.'">Learning bank</a>';
-				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login_english'.' ">Login</button></a>';							 														
+				echo '<a role="button" id="kirjautuminen" href="'.base_url().'sivu/login_english'.' ">Login</button></a>';
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "")
 			{
 				echo '<a role="button" id="takaisinprofiiliin" href="'.base_url().'sivu/members'.'">Oma profiili</a>';
-
 			}
 			elseif($this->session->userdata('is_logged_in') == 1 && $this->uri->segment(2) == "welcome_message_english")
 			{
@@ -147,11 +146,11 @@
 			}
 			elseif($this->uri->segment(2) == "register_successful")
 			{
-			
+
 			}
 			elseif($this->uri->segment(2) == "register_successful_english")
 			{
-			
+
 			}
 
 			elseif($this->uri->segment(2) == "")
@@ -166,7 +165,10 @@
 				echo '<a role="button" id="luotili" href="'.base_url().'sivu/register_english'.' ">Create an account</button></a>';
 				//echo '<a href ="'.base_url().'sivu/welcome_message"><img src="'.base_url().'pictures/Finnish_flag.png" class="col-md-5 col-md-offset-6" style="width:75px;height:30px;margin-right:-80px;"></a>';
 			}
-			
+
 	?>
-<body style="padding-top: 60px;">
-	</nav>
+</ul>
+</div><!--/.nav-collapse -->
+</div>
+</nav>
+<div style="padding-bottom: 60px;"></div>
