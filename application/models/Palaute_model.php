f3aa921d8e9b599cec7a04f0742c862fae8b0b87
Marko Knuutinen <marko.knuutinen@datanomit.local>
2017-01-23 12:52:05 +0200

    Commentointi

--- application/models/Palaute_model.php
+++ application/models/Palaute_model.php
@@ -1,6 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Palaute_model extends CI_Model {
+  //Luo Palautteen
   public function Palaute($User_id) {
     $data = array(
       'User_id'   =>    $User_id,
@@ -13,15 +14,18 @@ class Palaute_model extends CI_Model {
       return True;
     }
   }
+  //Hakee Palautteet
   public function hae_palaute() {
     $query = $this->db->get('palaute');
     return $query->result();
   }
+  //Poistaa Palautteen
   public function palaute_delete($id) {
     $this->db->where('id', $id);
     $this->db->delete('palaute');
     return true;
   }
+  //Vaihtaa Palautteen tilaa
   public function palaute_tila($data, $id) {
     $this->db->where('id', $id);
     $this->db->update('palaute', $data);
--- application/models/profile_model.php
+++ application/models/profile_model.php
@@ -2,6 +2,7 @@
 
 <?php
 class Profile_model extends CI_Model {
+//Luo Profiilin
   public function profile_setup() {
     $Profile = array(
         'User_id'       =>     filter_var($this->session->userdata('user_id'), FILTER_SANITIZE_STRING),
@@ -21,26 +22,31 @@ class Profile_model extends CI_Model {
       return true;
     }
   }
+//Hakee Harrastukset
   public function Get_harrastus($user_id) {
     $this->db->where('User_id',$user_id);
     $query = $this->db->get('harrastukset');
     return $query->result();
   }
+//Hakee Työhistorian
   public function Get_tyohistoria($user_id) {
     $this->db->where('User_id',$user_id);
     $query = $this->db->get('tyohistoria');
     return $query->result();
   }
+//Hakee Koulutukset
   public function Get_koulutus($user_id) {
     $this->db->where('User_id',$user_id);
     $query = $this->db->get('koulutus');
     return $query->result();
   }
+//Hakee Kortit
   public function Get_kortit($user_id) {
     $this->db->where('User_id',$user_id);
     $query = $this->db->get('kkortit');
     return $query->result();
   }
+//Hakee Metatiedot
   public function get_meta($user_id) {
     $this->db->select('*');
     $this->db->from('metatieto');
@@ -48,6 +54,7 @@ class Profile_model extends CI_Model {
     $query = $this->db->get();
     return $query->result();
   }
+//Hakee käyttäjän Profiilin
   public function get_profile($user_id) {
         $this->db->select('*');
         $this->db->from('profile');
@@ -55,6 +62,7 @@ class Profile_model extends CI_Model {
         $query = $this->db->get();
         return $query->result();
   }
+//Hakee Käyttäjän
   public function get_user($user_id) {
         $this->db->select('*');
         $this->db->from('users');
@@ -62,6 +70,7 @@ class Profile_model extends CI_Model {
         $query = $this->db->get();
         return $query->result();
   }
+//Luo Työhistorian
   public function tyohistoria($user_id) {
     $data = array(
         'User_id'       =>     $user_id,
@@ -76,6 +85,7 @@ class Profile_model extends CI_Model {
       return true;
     }
   }
+//Luo Koulutuksen
   public function koulutus($user_id) {
     $data = array(
         'User_id'         =>     $user_id,
@@ -90,6 +100,7 @@ class Profile_model extends CI_Model {
       return true;
     }
   }
+//Luo Kortin
   public function kortit($user_id) {
     $data = array(
         'User_id'     =>     $user_id,
@@ -102,31 +113,37 @@ class Profile_model extends CI_Model {
       return true;
     }
   }
+//Poistaa Koulutuksen
   public function koulutus_delete($id) {
     $this->db->where('id',$id);
     $this->db->delete('koulutus');
     return true;
   }
+//Poistaa Kortin
   public function kortit_delete($id) {
     $this->db->where('id',$id);
     $this->db->delete('kkortit');
     return true;
   }
+//Päivittää Koulutuksen
   public function koulutus_update($id,$data) {
     $this->db->where('id',$id);
     $this->db->update('koulutus',$data);
     return TRUE;
   }
+//Poistaa Työhistorian
   public function tyohistoria_delete($id) {
     $this->db->where('id',$id);
     $this->db->delete('tyohistoria');
     return true;
   }
+//Päivittää Työhistorian
   public function tyohistoria_update($id,$data) {
     $this->db->where('id',$id);
     $this->db->update('tyohistoria',$data);
     return TRUE;
   }
+//Luo Harrastuksen
   public function harrastus($user_id) {
     $data = array(
         'User_id'       =>     $user_id,
@@ -138,56 +155,67 @@ class Profile_model extends CI_Model {
       return true;
     }
   }
+//Vaihtaa Profiilin tilaa
   public function hide($user_id, $data) {
     $this->db->where('User_id',$user_id);
     $this->db->update('profile',$data);
     return TRUE;
   }
+//Päivittää Harrastuksen
   public function harrastus_update($id,$data) {
     $this->db->where('id',$id);
     $this->db->update('harrastukset',$data);
     return TRUE;
   }
+//Poistaa Harrastuksen
   public function harrastus_delete($id) {
     $this->db->where('id',$id);
     $this->db->delete('harrastukset');
     return true;
   }
+//Päivittää Profiilin
   public function update_profile($user_id,$data) {
     $this->db->where('User_id',$user_id);
     $this->db->update('profile',$data);
     return TRUE;
   }
+//Poistaa kaikki Metatiedot
   public function delete_all_meta($user_id) {
     $this->db->where('User_id',$user_id);
     $this->db->delete('metatieto');
     return true;
   }
+//Poistaa Metatiedon
   public function delete_meta($id) {
     $this->db->where('id',$id);
     $this->db->delete('metatieto');
     return true;
   }
+//Luo Metatiedon
   public function meta_add($data) {
     $insert = $this->db->insert('metatieto',$data);
     if ($insert) {
       return true;
     }
   }
+//Hakee Käyttäjän Palautteen
   public function hae_palaute_user($user_id) {
     $this->db->where('User_id', $user_id);
     $query = $this->db->get('palaute');
     return $query->result();
   }
+//Hakee kaikki Palautteet
   public function hae_palaute() {
     $query = $this->db->get('palaute');
     return $query->result();
   }
+//Poistaa Palautteen
   public function palaute_delete($id) {
     $this->db->where('id', $id);
     $this->db->delete('palaute');
     return true;
   }
+//Vaihtaa Palautteen tilan
   public function palaute_tila($data, $id) {
     $this->db->where('id', $id);
     $this->db->update('palaute', $data);
