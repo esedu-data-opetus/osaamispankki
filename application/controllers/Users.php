b7b0feb1ce29f40285b193ec115db521cc00b86d
JoonasMyllarinen <joonas.myllarinen15@gmail.com>
2017-02-24 11:07:29 +0200

    password reset fix

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -183,7 +183,7 @@ public function C_Key($key) {
   public function new_password($email, $rs2) {
     $this->form_validation->set_rules('password', 'Password', 'trim|required|max_length[50]|min_length[1]');
     $this->form_validation->set_rules('password_c', 'Confirm Password', 'trim|required|max_length[50]|min_length[1]|matches[password]');
-    if ($this->form_validation->run() === FALSE) {
+    if ($this->form_validation->run() == FALSE) {
       $data = array(
         'Pw_Email' => $email,
       );
