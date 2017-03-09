1113d13a481bc40c004106b5750ae06e0ac78f4e
Marko Knuutinen <marko.knuutinen@datanomit.local>
2017-03-09 08:54:43 +0200

    Ajax test

--- Ajax_test/index.php
+++ Ajax_test/index.php
@@ -1,8 +1,12 @@
+<html>
+<head>
 <script type="text/javascript" src="foodstore.js"></script>
-
+</head>
+<body onload="process()">
 <h2>Ajax test - Osaajapankki</h2>
-
 <h3>Ajax testi</h3>
 Enter Pizza, Makkara or Pihvi:
 <input type="text" id="userInput">
 <div id="underInput">
+</body>
+</html>
