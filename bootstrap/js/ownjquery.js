af121ff0292fbe51b896487faa8a55ae5840f40e
Marko Knuutinen <marko.knuutinen@datanomit.local>
2017-03-09 09:10:53 +0200

    Ajax test

--- bootstrap/js/ownjquery.js
+++ bootstrap/js/ownjquery.js
@@ -1,57 +1,5 @@
 //valitsee kaikki saman nimiset id:t
 //$('[id="id"]')
-
-var xmlHttp = createXmlHttpRequestObject();
-
-function createXmlHttpRequestObject() {
-  var xmlHttp;
-
-  if (window.ActiveXObject) {
-    try {
-      xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-    } catch(e) {
-      xmlHttp = false;
-    }
-  } else {
-    try {
-      xmlHttp = new XMLHttpRequest();
-    } catch(e) {
-      xmlHttp = false;
-    }
-  }
-  if (!xmlHttp) {
-    alert('Problem found!');
-  } else {
-    return xmlHttp;
-  }
-}
-
-function process() {
-  if (xmlHttp.readyState==0 || xmlHttp.readyState==4) {
-    food = encodeURIComponent(document.getElementById("userInput").value);
-    xmlHttp.open("GET", "foodstore.php?food=" + food, true);
-    xmlHttp.onreadystatechange = handleServerResponse;
-    xmlHttp.send(null);
-  } else {
-    setTimeout('process()',1000);
-  }
-}
-
-function handleServerResponse() {
-  if (xmlHttp.readyState==4) {
-    if (xmlHttp.status==200) {
-      xmlResponse = xmlHttp.responseXML;
-      xmlDocumentElement = xmlResponse.documentElement;
-      message = xmlDocumentElement.firstChild.data;
-      document.getElementById("underInput").innerHTML = '<span style="color: blue">' + message + '</span>';
-      setTimeout('process()',1000);
-    } else {
-      alert('Found an error D:');
-    }
-  }
-}
-
-
 function myResize() {
   var w = window.outerWidth;
   var h = window.outerHeight;
