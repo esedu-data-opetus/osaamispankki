1e7c7ebd815af40bc67221b8d064525fa7cc09f9
Marko-K <marko.knuutinen@esedulainen.fi>
2017-03-10 14:13:51 +0200

    FPDF update

--- application/views/PDF/index.php
+++ application/views/PDF/index.php
@@ -1,17 +1,9 @@
 <?php
-foreach($Prof_Info as $Prof) {
-  $Profile = array(
-    "Name"    =>  $Prof->F_Name." ".$Prof->L_Name,
-    "Sposti"  =>  $Prof->Sposti,
-    "Osoite"  =>  $Prof->Osoite,
-  );
-}
 require('fpdf.php');
 
 class PDF extends FPDF {
 // Page header
 function Header() {
-    // Logo
     // Arial bold 15
     $this->SetFont('Arial','B',15);
     // Title
@@ -31,17 +23,31 @@ function Footer() {
 }
 }
 
+foreach($Prof_Info as $Prof) {
+  $Profile = array(
+    "Name"      =>  $Prof->F_Name." ".$Prof->L_Name,
+    "Sposti"    =>  $Prof->Sposti,
+    "Osoite"    =>  $Prof->Osoite,
+    "Posti_Num" =>  $Prof->Posti_Num,
+    "Puh_Num"   =>  $Prof->Puh_Num,
+  );
+}
 // Instanciation of inherited class
 $pdf = new PDF();
 $title = $Profile['Name'];
 $pdf->SetTitle($title);
 $pdf->AliasNbPages();
 $pdf->AddPage();
+$ht = 10;
 $pdf->SetFont('Arial','i',15);
 $pdf->Cell(0,10,'        Nimi:                    '.$Profile['Name']);
-$pdf->Ln(6);
+$pdf->Ln($ht);
 $pdf->Cell(0,10,'        Osoite:                 '.$Profile['Osoite']);
-$pdf->Ln(6);
+$pdf->Ln($ht);
+$pdf->Cell(0,10,'        Postinumero:       '.$Profile['Posti_Num']);
+$pdf->Ln($ht);
+$pdf->Cell(0,10,'        Puhelin_num:      '.$Profile['Puh_Num']);
+$pdf->Ln($ht);
 $pdf->Cell(0,10,'        Sahkoposti:         '.$Profile['Sposti']);
 $pdf->Output();
 ?>
