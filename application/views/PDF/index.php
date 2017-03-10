88f5ca94913b1109139a960aab17ed398b567347
Marko-K <marko.knuutinen@esedulainen.fi>
2017-03-10 12:48:01 +0200

    FPDF update

--- application/views/PDF/index.php
+++ application/views/PDF/index.php
@@ -1,79 +1,47 @@
 <?php
+foreach($Prof_Info as $Prof) {
+  $Profile = array(
+    "Name"    =>  $Prof->F_Name." ".$Prof->L_Name,
+    "Sposti"  =>  $Prof->Sposti,
+    "Osoite"  =>  $Prof->Osoite,
+  );
+}
 require('fpdf.php');
 
-class PDF extends FPDF
-{
-function Header()
-{
-    global $title;
-    $title = "Hello World!";
+class PDF extends FPDF {
+// Page header
+function Header() {
+    // Logo
     // Arial bold 15
     $this->SetFont('Arial','B',15);
-    // Calculate width of title and position
-    $w = $this->GetStringWidth($title)+6;
-    $this->SetX((210-$w)/2);
-    // Colors of frame, background and text
-    $this->SetDrawColor(0,0,0);
-    $this->SetFillColor(300,300,9000);
-    $this->SetTextColor(0,0,0);
-    // Thickness of frame (1 mm)
-    $this->SetLineWidth(1);
     // Title
-    $this->Cell($w,9,$title,1,1,'C',true);
+    $this->Cell(30,10,'Henkilotiedot');
     // Line break
-    $this->Ln(10);
+    $this->Ln();
 }
 
-function Footer()
-{
+// Page footer
+function Footer() {
     // Position at 1.5 cm from bottom
     $this->SetY(-15);
     // Arial italic 8
     $this->SetFont('Arial','I',8);
-    // Text color in gray
-    $this->SetTextColor(128);
     // Page number
-    $this->Cell(0,10,'Page '.$this->PageNo(),0,0,'C');
-}
-
-function ChapterTitle($num, $label)
-{
-    // Arial 12
-    $this->SetFont('Arial','',12);
-    // Background color
-    $this->SetFillColor(200,220,255);
-    // Title
-    $this->Cell(0,6,"Chapter $num : $label",0,1,'L',true);
-    // Line break
-    $this->Ln(4);
-}
-
-function ChapterBody($file)
-{
-    // Read text file
-    // Times 12
-    $this->SetFont('Times','',12);
-    // Output justified text
-    // Line break
-    $this->Ln();
-    // Mention in italics
-    $this->SetFont('','I');
-    $this->Cell(0,5,'(Work in progress)');
-}
-
-function PrintChapter($num, $title, $file)
-{
-    $this->AddPage();
-    $this->ChapterTitle($num,$title);
-    $this->ChapterBody($file);
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 }
 }
 
+// Instanciation of inherited class
 $pdf = new PDF();
-$title = '20000 Leagues Under the Seas';
+$title = $Profile['Name'];
 $pdf->SetTitle($title);
-$pdf->SetAuthor('Jules Verne');
-$pdf->PrintChapter(1,'A RUNAWAY REEF','20k_c1.txt');
-$pdf->PrintChapter(2,'THE PROS AND CONS','20k_c2.txt');
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','i',15);
+$pdf->Cell(0,10,'        Nimi:                    '.$Profile['Name']);
+$pdf->Ln(6);
+$pdf->Cell(0,10,'        Osoite:                 '.$Profile['Osoite']);
+$pdf->Ln(6);
+$pdf->Cell(0,10,'        Sahkoposti:         '.$Profile['Sposti']);
 $pdf->Output();
 ?>
